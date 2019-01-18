f5e99e330f47e503d8f24349260f622206f84091
Paul Wilson <sf@statusfailed.com>
2019-01-18 13:03:11 +0000

    comment smc example better

--- asm/smc.asm
+++ asm/smc.asm
@@ -8,6 +8,8 @@ SECTION .text
 ;
 ; NOTE: extremely brittle, because it bakes in exact addresses when doing
 ; self-modification. Nasm probably has a way to make this nicer?
+;
+; When using elf-from-scratch, main will be loaded into memory at 0x400078.
 main:
   ; put "hur\n" on the stack
   mov rax, 0x0a727568
@@ -20,23 +22,26 @@ main:
   mov rdx, 4  ; len("hur\n")
   syscall;
 
+  ; Pop the stack
   ; NOTE: on lines 37+38, this & the following instruction are overwritten
   ; with a 2-byte relative JMP to line 44 (the exit syscall @ "end").
   pop rax
 
-  ; modify code (x86-64 stores 4-byte constant @ main+1, so we modify the first
-  ; byte.
-  ; change to write "dur\n" instead, and then jump there + execute it.
+  ; modify code (x86-64 encodes the 4-byte constant of the first instruction @
+  ; address main+1, so we modify the first byte to be "d" instead of "h".
   mov byte [0x400079], 0x64 ; WARNING: more baked in offsets
 
   ; modify "pop rax" to instead jump to end.
-  ; here we directly write the instruction encode:
-  ;   * EB for JMP relative,
-  ;   * 0x16 for 8-bit relative offset
-  ; target: b8, from 92
+  ; here we directly write the instruction encoding:
+  ;   * 0xEB for JMP relative,
+  ;   * 0x19 for 8-bit relative offset
+  ; see here for reference: https://www.felixcloutier.com/x86/jmp
   mov byte [0x400092], 0xEB ; opcode for relative jump
   mov byte [0x400093], 0x19 ; relative offset (calculated using gdb)
 
+  ; now jump back to main and execute the new (modified) code:
+  ;   * print "dur"
+  ;   * jump directly to end
   jmp main
 
 end:
