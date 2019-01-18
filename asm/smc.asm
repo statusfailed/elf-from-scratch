c32937a5a9f7d96a50881dfd34cb5679e4d47f53
Paul Wilson <sf@statusfailed.com>
2019-01-18 12:58:28 +0000

    make self modifying example more fun (overwrite instructions!)

--- asm/smc.asm
+++ asm/smc.asm
@@ -20,18 +20,23 @@ main:
   mov rdx, 4  ; len("hur\n")
   syscall;
 
+  ; NOTE: on lines 37+38, this & the following instruction are overwritten
+  ; with a 2-byte relative JMP to line 44 (the exit syscall @ "end").
   pop rax
-
-  ;;;; if line 10 writes "dur", skip to end.
-  ; load 2nd byte of program + examine
-  mov al, byte [0x400079] ; address of main if you compile with main.py (lool)
-  cmp al, 0x64 ; rax == 'd' ?
-  je end
 
   ; modify code (x86-64 stores 4-byte constant @ main+1, so we modify the first
   ; byte.
   ; change to write "dur\n" instead, and then jump there + execute it.
   mov byte [0x400079], 0x64 ; WARNING: more baked in offsets
+
+  ; modify "pop rax" to instead jump to end.
+  ; here we directly write the instruction encode:
+  ;   * EB for JMP relative,
+  ;   * 0x16 for 8-bit relative offset
+  ; target: b8, from 92
+  mov byte [0x400092], 0xEB ; opcode for relative jump
+  mov byte [0x400093], 0x19 ; relative offset (calculated using gdb)
+
   jmp main
 
 end:
